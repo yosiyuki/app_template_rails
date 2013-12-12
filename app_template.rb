b42ff843f7ab150ddf18143999a1e2bc9b6fcde4
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2013-12-12 15:57:36 +0700

    add current gems

---
+++ app_template.rb
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'thin'
+gem 'slim'
+gem 'exception_notification'
+
+group :development, :test do
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3'
+  gem 'slim-rails'
+  gem 'rspec-rails'
+  gem 'factory_girl'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+end
+
+group :assets do
+  gem 'sass-rails', '~> 4.0.0'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'coffee-rails', '~> 4.0.0'
+end
+
+
+group :production do
+  gem 'rails_log_stdout',           github: 'heroku/rails_log_stdout'
+  gem 'rails3_serve_static_assets', github: 'heroku/rails3_serve_static_assets'
+  gem 'pg'
+  gem 'dalli'
+end
