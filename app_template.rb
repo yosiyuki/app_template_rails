e031ba8aaa47ff0f1696dfdeefb97777e673f0e4
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2013-12-14 14:57:40 +0700

    add initial files

--- app_template.rb
+++ app_template.rb
@@ -1,15 +1,18 @@
+repo = 'https://raw.github.com/yosiyuki/app_template_rails/master'
+
 gem 'thin'
 gem 'slim'
 gem 'exception_notification'
 
 gem_group :development, :test do
-  # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
   gem 'slim-rails'
   gem 'rspec-rails'
   gem 'factory_girl'
   gem 'factory_girl_rails'
   gem 'database_cleaner'
+  gem 'pry-rails'
+  gem 'pry-byebug'
 end
 
 gem_group :assets do
@@ -25,3 +28,27 @@ gem_group :production do
   gem 'pg'
   gem 'dalli'
 end
+
+remove_file "public/index.html"
+remove_file "app/view/layouts/application.html.erb"
+get "#{repo}/app/view/layouts/application.html.slim", "app/view/layouts/application.html.slim"
+
+file 'Procfile.rb', <<-CODE
+  web: bundle exec thin start -p $PORT -e $RACK_ENV
+CODE
+
+file '.env', <<-CODE
+  PORT=3333
+  RACK_ENV=development
+CODE
+
+run_bundle
+
+git :init
+git add: '.'
+git commit: "-m 'initial commit'"
+
+generate 'rspec:install'
+
+git add: '.'
+git commit: "-m 'rspec install'"
